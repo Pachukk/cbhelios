bb25ff8dcbc63ca1ffd3579a858e6703cff79d6c
PachGit <108862443+PachGit@users.noreply.github.com>
2022-08-23 14:08:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -70,7 +70,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
@@ -195,7 +195,7 @@ help_string = f'''
 Hei, Need Help!!
 '''
 help = telegraph.create_page(
-        title='Helios-Mirror Help',
+        title='Mirror Help',
         content=help_string_telegraph + sudo_help_string,
     )["path"]
 
